75e6338a8d57985190a37554505bc1ef154cd7c8
TimoBel <62485715+TimoBel@users.noreply.github.com>
2022-07-14 20:17:24 -0300

    Add files via upload

---
+++ clases.py
@@ -0,0 +1,119 @@
+class Cliente:
+    def __init__(self,nombre, apellido, numero, dni, transacciones):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.numero = numero
+        self.dni = dni
+        self.transacciones = transacciones
+    
+    def retirar(cantidad):
+        pass
+
+ 
+
+
+class Classic(Cliente):
+    comision = 0.01
+    maximoDiario = 10000
+    transferenciaMaxima = 150000
+
+    def puedeCrearChequera(self, cantChequeras):
+        return "Un cliente classic no puede tener chequera"
+
+    def puedeCrearTarjeta(self, cantTarjetas):
+        return "Un cliente classic no puede tener tarjeta de credito"
+
+    def puedeComprarDolar(self, monto, saldoEnCuenta):
+        return "Un cliente classic no puede comprar dolares"
+
+    def puedeHacerTransferencia(self, saldoEnCuenta, monto):
+        if saldoEnCuenta >= (monto + monto*self.comision):
+            return True
+        return "El saldo en cuenta es menor al monto requerido para transferir"
+    
+    def puedeRecibirTransferencia(self, monto):
+        if monto > self.transferenciaMaxima:
+            return "El monto es mayor al limite por transferencias ($150000)"
+
+    
+    def puedeRetirar(self, monto, saldoEnCuenta, cupoDiarioRestante):
+        if monto > saldoEnCuenta:
+            return "El monto execede el dinero en cuenta"
+        if monto > cupoDiarioRestante:
+            return "El monto execede el cupo diario restante"
+        return True
+    
+   
+
+    
+ 
+class Gold(Cliente):
+    comision = 0.005
+    maximoDiario = 20000
+    descubierto = 10000
+    transferenciaMaxima = 500000
+
+    def puedeCrearChequera(self, cantChequeras):
+        if(cantChequeras < 1):
+            return True
+        return "Ya superas el limite de chequeras (1)"
+
+
+    def puedeCrearTarjeta(self, cantTarjetas):
+        if(cantTarjetas < 1):
+            return True
+        return "Ya superas el limite de tajetas de credito (1)"
+    
+    def puedeHacerTransferencia(self, saldoEnCuenta, monto):
+        if saldoEnCuenta >= (monto + monto*self.comision):
+            return True
+        return "El saldo en cuenta es menor al monto requerido para transferir"
+    
+    def puedeRecibirTransferencia(self, monto):
+        if monto > self.transferenciaMaxima:
+            return "El monto es mayor al limite por transferencias ($500000)"
+        
+    def puedeRetirar(self, monto, saldoEnCuenta, cupoDiarioRestante):
+        if monto > (saldoEnCuenta + self.descubierto):
+            return "El monto execede el dinero en cuenta"
+        if monto > cupoDiarioRestante:
+            return "El monto execede el cupo diario restante"
+        return True
+
+    def puedeComprarDolar(self, monto, saldoEnCuenta):
+        if saldoEnCuenta >= monto:
+            return True
+        return "No tiene saldo suficiente"
+
+
+class Black(Cliente):
+    maximoDiario = 100000
+    descubierto = 10000
+
+    def puedeCrearChequera(self, cantChequeras):
+        if(cantChequeras < 2):
+            return True
+        return "Ya superas el limite de chequeras (2)"
+
+    def puedeCrearTarjeta(self, cantTarjetas):
+        if(cantTarjetas < 5):
+            return True
+        return "Ya superas el limite de tajetas de credito (5)"
+
+    def puedeHacerTransferencia(self, saldoEnCuenta, monto):
+        if saldoEnCuenta >= monto:
+            return True
+        return "El saldo en cuenta es menor al monto requerido para transferir"
+    
+    def puedeRetirar(self, monto, saldoEnCuenta, cupoDiarioRestante):
+        if monto > (saldoEnCuenta + self.descubierto):
+            return "El monto execede el dinero en cuenta"
+        if monto > cupoDiarioRestante:
+            return "El monto execede el cupo diario restante"
+        return True
+
+    def puedeComprarDolar(self, monto, saldoEnCuenta):
+        if saldoEnCuenta >= monto:
+            return True
+        return "No tiene saldo suficiente"
+    
---
+++ funciones.py
@@ -0,0 +1,92 @@
+import json
+from jsonschema import ValidationError, validate
+from jinja2 import Environment, PackageLoader, select_autoescape
+from clases import *
+
+def leerJSON(nombreJSON):
+    try:
+        with open(nombreJSON, "r") as file:
+            tps = json.load(file)
+            validate(instance=tps, schema=schema)
+            return tps
+    except FileNotFoundError:
+        print("No se encontró el archivo JSON")
+
+    except ValidationError as e:
+        print("El JSON esta mal formado,", e)
+
+def handlerTipoCliente(tipo, tps):
+        if tipo=="CLASSIC":
+            cliente = Classic(tps["nombre"], tps["apellido"], tps["numero"], tps["dni"],tps["transacciones"])
+        if tipo == "BLACK":
+            cliente = Black(tps["nombre"], tps["apellido"], tps["numero"], tps["dni"],tps["transacciones"])
+        if tipo == "GOLD":
+            cliente = Gold(tps["nombre"], tps["apellido"], tps["numero"], tps["dni"],tps["transacciones"])
+        return cliente
+
+def handlerTipoTransaccion(transaccion, cliente):
+    if transaccion["tipo"] == "RETIRO_EFECTIVO_CAJERO_AUTOMATICO":
+        return cliente.puedeRetirar(transaccion["monto"], transaccion["saldoEnCuenta"], transaccion["cupoDiarioRestante"])
+
+    if transaccion["tipo"] == "ALTA_CHEQUERA":
+        return cliente.puedeCrearChequera(transaccion["totalChequerasActualmente"])
+    
+    if transaccion["tipo"] == "ALTA_TARJETA_CREDITO":
+        return cliente.puedeCrearTarjeta(transaccion["totalTarjetasDeCreditoActualmente"])
+
+    if transaccion["tipo"] == "COMPRA_DOLAR":
+        return cliente.puedeComprarDolar(transaccion["monto"], transaccion["saldoEnCuenta"])
+
+    if transaccion["tipo"] == "TRANSFERENCIA_ENVIADA":
+        return cliente.puedeHacerTransferencia(transaccion["saldoEnCuenta"], transaccion["monto"])
+
+    if transaccion["tipo"] == "TRANSFERENCIA_RECIBIDA":
+        return cliente.puedeRecibirTransferencia(transaccion["monto"])
+    
+
+def generarHTML(tps, rechazo):
+    env = Environment(
+    loader=PackageLoader("paquete"),
+    autoescape=select_autoescape()
+    )
+
+    template = env.get_template("template.html")
+
+    with open("reporte.html", "w") as file:
+        file.write(template.render(tps=tps, rechazo=rechazo))
+
+
+
+
+
+
+
+
+schema = {
+    "type": "object",
+    "properties":{
+        "numero": {"type": "number"},
+        "nombre": {"type": "string"},
+        "apellido": {"type": "string"},
+        "DNI": {"type": "string"},
+        "tipo": {"enum": ["BLACK", "CLASSIC", "GOLD"]},
+        "transacciones": {
+            "type": "array",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "estado": {"enum": ["ACEPTADA", "RECHAZADA"]},
+                    "tipo": {"type": "string"},
+                    "cuentaNumero": {"type": "number"},
+                    "cupoDiarioRestante": {"type": "number"},
+                    "monto": {"type": "number"},
+                    "fecha": {"type": "string"},
+                    "numero": {"type": "number"},
+                    "saldoEnCuenta": {"type": "number"},
+                    "totalTarjetasDeCreditoActualmente": {"type": "number"},
+                    "totalChequerasActualmente": {"type": "number"}
+                }
+            }
+        }
+    }
+}
